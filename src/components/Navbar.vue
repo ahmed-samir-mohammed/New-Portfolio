5d6c2027e642b33cf9a3b84c9f13c7671b5271d2
ahmed-samir-mohammed <samierahmed37@gmail.com>
2019-11-07 17:13:38 +0200

    Add Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <b-navbar variant="faded" type="light">
+        <router-link :to="{name: 'Home'}">
+            <b-navbar-brand tag="h1" class="mb-0">
+                <span>A</span>hmed - <span>S</span>amir
+            </b-navbar-brand>
+        </router-link>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style lang="scss">
+    .navbar-brand {
+        display: flex;
+        align-items: center;
+        text-decoration: none;
+        span {
+            font-size: 2rem
+        }
+    }
+</style>
